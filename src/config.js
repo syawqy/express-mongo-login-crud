9d6466e49f62f5dd244ce50b4d7864cda2a3d3eb
syawqy <xfuadi@gmail.com>
2022-07-07 13:07:36 +0700

    test config

--- src/config.js
+++ src/config.js
@@ -4,24 +4,24 @@ const Joi = require('joi');
 
 dotenv.config({ path: path.join(__dirname, '../.env') });
 
-const envVarsSchema = Joi.object()
-  .keys({
-    NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
-    PORT: Joi.number().default(3000),
-    MONGODB_URL: Joi.string().required().description('Mongo DB url'),
-    JWT_SECRET: Joi.string().required().description('JWT secret key'),
-    JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
-    JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
-    JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
-      .default(10)
-      .description('minutes after which reset password token expires'),
-    JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number()
-      .default(10)
-      .description('minutes after which verify email token expires')
-  })
-  .unknown();
+// const envVarsSchema = Joi.object()
+//   .keys({
+//     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
+//     PORT: Joi.number().default(3000),
+//     MONGODB_URL: Joi.string().required().description('Mongo DB url'),
+//     JWT_SECRET: Joi.string().required().description('JWT secret key'),
+//     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
+//     JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
+//     JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
+//       .default(10)
+//       .description('minutes after which reset password token expires'),
+//     JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number()
+//       .default(10)
+//       .description('minutes after which verify email token expires')
+//   })
+//   .unknown();
 
-const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+// const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
